baa68807cadd1b9cd66bd9101e091b4a05f4c679
Bolt-sudo <boltfinisher@gmail.com>
2026-05-05 13:08:12 +0530

    fix: use port 465 for Gmail SMTP

--- utils/notificationService.js
+++ utils/notificationService.js
@@ -5,7 +5,9 @@ const Submission = require('../models/Submission');
 
 const createTransporter = () => {
   return nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
