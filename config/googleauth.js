0f0fb353f3bcfed3fab41b5f75afb078ed9d89db
Bolt-sudo <boltfinisher@gmail.com>
2026-05-05 09:51:29 +0530

    fix: switch to service account auth

--- config/googleauth.js
+++ config/googleauth.js
@@ -1,19 +1,29 @@
 const { google } = require('googleapis');
 
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  'http://localhost:5000/oauth2callback'
-);
-
-oauth2Client.setCredentials({
-  refresh_token: process.env.GOOGLE_REFRESH_TOKEN
-});
-
-const auth = {
-  getClient: async () => oauth2Client
+const serviceAccountKey = {
+  type: 'service_account',
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+  universe_domain: 'googleapis.com',
 };
 
-console.log('REFRESH TOKEN loaded:', !!process.env.GOOGLE_REFRESH_TOKEN);
+const auth = new google.auth.GoogleAuth({
+  credentials: serviceAccountKey,
+  scopes: [
+    'https://www.googleapis.com/auth/forms.body',
+    'https://www.googleapis.com/auth/forms.responses.readonly',
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file',
+  ],
+});
+
+console.log('Service account loaded:', !!process.env.GOOGLE_CLIENT_EMAIL);
 
 module.exports = auth;
