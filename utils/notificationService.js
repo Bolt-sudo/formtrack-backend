d752e7224951602df0184c9e216b593358e7e482
Bolt-sudo <boltfinisher@gmail.com>
2026-05-05 14:06:40 +0530

    fix: revert to gmail service for SMTP

--- utils/notificationService.js
+++ utils/notificationService.js
@@ -5,9 +5,7 @@ const Submission = require('../models/Submission');
 
 const createTransporter = () => {
   return nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
